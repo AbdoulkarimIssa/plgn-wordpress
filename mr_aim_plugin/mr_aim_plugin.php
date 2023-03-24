53381dc44bb6ad513f20b11e1e691e916c3e5220
maxime richaudeau <maxime.richaudeau1@gmail.com>
2023-03-24 14:29:06 +0100

    Init plugin wp

---
+++ mr_aim_plugin/mr_aim_plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Spotify Plugin by Issa Abdoulkarim et Maxime RICHAUDEAU
+Plugin URI: https://github.com/AbdoulkarimIssa/plgn-wordpress
+Description: Plugin de gestion spotify.
+Author: Issa&Maxime
+Version: 1.0
+*/
